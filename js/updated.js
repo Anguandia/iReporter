20c78d4d6668e69a5686f4d4a9e7bd06238b9f21
Anguandia <mikeanguandia@gmail.com>
2018-12-21 13:16:00 +0300

    change update simulation

---
+++ js/updated.js
@@ -0,0 +1,4 @@
+function updated(){
+    document.getElementById('status').innerHTML='document.getElementsByClassName("radio").checked.name';
+    document.getElementById('status').style.color='red';
+}
