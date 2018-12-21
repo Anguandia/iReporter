03e42d3d8965ae46b8e5f71b5348d55f4f06456f
Anguandia <mikeanguandia@gmail.com>
2018-12-21 13:36:41 +0300

    modify uppdate function

--- js/updated.js
+++ js/updated.js
@@ -1,5 +1,10 @@
 function updated(){
-    document.getElementById('status').innerHTML=document.getElementsByClassName("radio").checked.name;
-    document.getElementById('status').style.color='gold';
+    if(document.getElementById('resolved').checked=='true'){
+        document.getElementById('status').innerHTML='resolved';
+        document.getElementById('status').style.color='green';
+    }else{
+        document.getElementById('status').innerHTML='rejected';
+        document.getElementById('status').style.color='red';
+    }
     document.getElementById("section3").innerHTML="<p id='feedback'>Action Successful!</p>";
 }
